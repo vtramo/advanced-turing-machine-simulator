8525e41b25dcd4ddea34be1dc016a67b9912830e
Vincenzo Tramo <vv.tramo@gmail.com>
2023-12-12 11:23:33 +0100

    feat(engine): add Instruction record
    - Instruction is a record enclosing a Turing machine instruction. Based on the current state and the current symbols contained in StateAndSymbols, the machine executes the specified transition (Transition).
    - Implemented static factory method `of` that constructs an Instruction object based on input left string (to form state-and-symbol pairing) and right string (to form transition).

---
+++ src/main/java/com/github/vtramo/turingmachine/engine/Instruction.java
@@ -0,0 +1,29 @@
+package com.github.vtramo.turingmachine.engine;
+
+public record Instruction(StateAndSymbols stateAndSymbols, Transition transition) {
+
+    public static Instruction of(final String left, final String right) {
+        final String[] leftArray = left.split(",");
+        final String[] rightArray = right.split(",");
+
+        final String state = leftArray[0].strip();
+        final String nextState = rightArray[0].strip();
+
+        final char[] symbols = new char[leftArray.length - 1];
+        for (int i = 1; i < leftArray.length; i++) {
+            final char symbol = leftArray[i].strip().toCharArray()[0];
+            symbols[i - 1] = symbol;
+        }
+
+        final Move[] moves = new Move[symbols.length];
+        for (int i = 1, j = 0; i < rightArray.length; i += 2, j++) {
+            final char symbol = rightArray[i].strip().toCharArray()[0];
+            final Direction direction = Direction.of(rightArray[i + 1].strip());
+            moves[j] = Move.of(symbol, direction);
+        }
+
+        final StateAndSymbols stateAndSymbols = StateAndSymbols.of(state, symbols);
+        final Transition transition = Transition.of(nextState, moves);
+        return new Instruction(stateAndSymbols, transition);
+    }
+}
